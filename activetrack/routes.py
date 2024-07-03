9e6c47d15f8cb65c581cc84be2605ac19d86db21
Andrew Collins <andrewcollings@outlook.com>
2024-07-03 17:26:50 +0000

    Add routes for login, logout, sign-up

--- activetrack/routes.py
+++ activetrack/routes.py
@@ -6,3 +6,18 @@ from activetrack.models import User, Exercise, Activity, Comment
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/login")
+def login():
+    return "<p>Login</p>"
+
+
+@app.route('/logout')
+def logout():
+    return "<p>Logout</p>"
+
+
+@app.route('/sign-up')
+def sign_up():
+    return "<p>Sign Up</p>"
