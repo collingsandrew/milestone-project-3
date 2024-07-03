07bc17f33b78637f0343e024654e7ca1472cfb31
Andrew Collins <andrewcollings@outlook.com>
2024-07-03 20:01:46 +0000

    Fix code error

--- activetrack/routes.py
+++ activetrack/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, request, flash
 from activetrack import app, db
 from activetrack.models import User, Exercise, Activity, Comment
 
@@ -8,7 +8,7 @@ def home():
     return render_template("home.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
     return render_template("login.html")
 
@@ -18,6 +18,28 @@ def logout():
     return "<p>Logout</p>"
 
 
-@app.route('/sign-up')
+@app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
+
+    if request.method == 'POST':
+        username = request.form.get('username')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(username) < 3:
+            flash('Username must be atleast 3 characters.',
+            category='error')
+        elif len(email) < 4:
+            flash('Email is too short.',
+            category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.',
+            category='error')
+        elif len(password1) < 7:
+            flash('Password is too short, please enter more than 7 characters.',
+            category='error')
+        else:
+            flash('Signed up!', category='success')
+
     return render_template("sign_up.html")
