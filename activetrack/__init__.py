efc93de4f992314d6153381c1234d0a3bdba164c
Andrew Collins <andrewcollings@outlook.com>
2024-07-07 16:40:28 +0000

    Add to __init.py__ import LoginManager and add code and decorator to allow user login

--- activetrack/__init__.py
+++ activetrack/__init__.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
 
 if os.path.exists("env.py"):
     import env  # noqa
@@ -10,6 +12,19 @@ app = Flask(__name__)
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
 
+
 db = SQLAlchemy(app)
 
+
 from activetrack import routes  # noqa
+from activetrack.models import User # noqa
+
+
+login_manager = LoginManager()
+login_manager.login_view = 'routes.home'
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
