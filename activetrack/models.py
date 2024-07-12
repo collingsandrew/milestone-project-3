c1dc1ebfee05d63c7d39dfb62ddcf79f9be233a6
Andrew Collins <andrewcollings@outlook.com>
2024-07-12 11:27:27 +0000

    Update refactor code pep8 compliance

--- activetrack/models.py
+++ activetrack/models.py
@@ -13,16 +13,20 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), unique=True, nullable=False)
     email = db.Column(db.String(150), unique=True, nullable=False)
     password = db.Column(db.String(150), nullable=False)
-    activities = db.relationship('Activity', backref='user', lazy=True, cascade="all, delete")
+    activities = db.relationship(
+        'Activity', backref='user', lazy=True, cascade="all, delete")
 
     def __repr__(self):
-        return f"#{self.id}|username:{self.username}| email:{self.email}"
+        return (f"#{self.id} |"
+            f"username:{self.username} |"
+            f"email: {self.email}")
 
 
 class Activity(db.Model):
     # schema for the Activity model
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
     workout_type = db.Column(db.String, nullable=False)
     exercise_name = db.Column(db.String, nullable=False)
     reps = db.Column(db.Integer)
@@ -31,21 +35,32 @@ class Activity(db.Model):
     weight = db.Column(db.Numeric)
     duration = db.Column(db.Numeric)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now())
-    comments = db.relationship('Comment', backref='activity', lazy=True, cascade="all, delete")
+    comments = db.relationship(
+        'Comment', backref='activity', lazy=True, cascade="all, delete")
 
     def __repr__(self):
-        return (f"<Activity id={self.id}|exercise_name={self.exercise_name}|reps={self.reps}|distance={self.distance}"
-                f"sets={self.sets}|duration={self.duration}|created_at={self.created_at}>")
+        return (f"<Activity id={self.id} |"
+            f"exercise_name={self.exercise_name} |"
+            f"reps={self.reps} |"
+            f"distance={self.distance} |"
+            f"sets={self.sets} |"
+            f"duration={self.duration} |"
+            f"created_at={self.created_at}>")
 
 
 class Comment(db.Model):
     # schema for the Comment model
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete="CASCADE"), nullable=False)
-    activity_id = db.Column(db.Integer, db.ForeignKey('activity.id', ondelete="CASCADE"), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user.id', ondelete="CASCADE"), nullable=False)
+    activity_id = db.Column(db.Integer, db.ForeignKey(
+        'activity.id', ondelete="CASCADE"), nullable=False)
     comment_text = db.Column(db.Text, nullable=False)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now())
-    user = db.relationship('User', backref=db.backref('comment', lazy='dynamic'))
+    user = db.relationship('User', backref=db.backref(
+        'comment', lazy='dynamic'))
 
     def __repr__(self):
-        return f"#{self.id}|comment_text:{self.comment_text}|created_at:{self.created_at}"
+        return (f"#{self.id} |"
+        f"comment_text:{self.comment_text} |"
+        f"created_at:{self.created_at}")
--- activetrack/routes.py
+++ activetrack/routes.py
@@ -1,9 +1,11 @@
-from flask import render_template, request, flash, redirect, url_for, jsonify
-from activetrack import app, db
-from activetrack.models import User, Activity, Comment
+import json
+
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, login_required, logout_user, current_user
-import json
+from flask import render_template, request, flash, redirect, url_for, jsonify
+
+from activetrack import app, db
+from activetrack.models import User, Activity, Comment
 
 
 @app.route("/")
@@ -70,31 +72,39 @@ def sign_up():
         elif user_email:
             flash('Email already exists', category='error')
         elif len(username) < 3:
-            flash('Username must be atleast 3 characters.',
-            category='error')
+            flash(
+                'Username must be atleast 3 characters.',
+                category='error')
         elif len(email) < 4:
-            flash('Email is too short.',
-            category='error')
+            flash('Email is too short.', category='error')
         elif password1 != password2:
-            flash('Passwords do not match.',
-            category='error')
+            flash('Passwords do not match.', category='error')
         elif len(password1) < 7:
-            flash('Password is too short, please enter more than 7 characters.',
-            category='error')
+            flash(
+                'Password is too short. Must be 7 plus characters.',
+                category='error')
         else:
             try:
                 # attempt to add new user to the database
-                new_user = User(username=username, email=email, password=generate_password_hash(password1, method='sha256'))
+                new_user = User(
+                    username=username,
+                    email=email, 
+                    password=generate_password_hash(
+                        password1, method='sha256'
+                    )
+                )
                 db.session.add(new_user)
                 db.session.commit()
-                flash('Successfully signed up! Please log in', category='success')
+                flash('Successfully signed up! Please log in',
+                    category='success')
                 return redirect(url_for('login'))
             except Exception as e:
                 # if an error occurs
                 # cancel any potential changes made to the database
                 db.session.rollback()
                 # catch the error and flash a message
-                flash(f'Error, failed to sign up: {str(e)}', category='error')
+                flash(f'Error, failed to sign up: {str(e)}',
+                    category='error')
     return render_template("sign_up.html", user=current_user)
 
 
@@ -103,11 +113,18 @@ def sign_up():
 def diary():
     try:
         # attempt to load user activities
-        user_activities = db.session.query(Activity).filter_by(user_id=current_user.id).all()
-        return render_template("diary.html", user=current_user, activities=user_activities)
+        user_activities = db.session.query(Activity).filter_by(
+            user_id=current_user.id
+        ).all()
+        return render_template(
+            "diary.html",
+            user=current_user,
+            activities=user_activities
+        )
     except Exception as e:
         # catch the error and flash a message, redirect to home page
-        flash(f'Error, failed to load activities: {str(e)}', category='error')
+        flash(f'Error, failed to load activities: {str(e)}',
+            category='error')
         return redirect(url_for('home'))
 
 
@@ -122,11 +139,16 @@ def add_activity():
                 user_id=current_user.id,
                 workout_type=request.form.get("workout_type"),
                 exercise_name=request.form.get("exercise_name"),
-                reps=int(request.form.get("reps", 0)) if request.form.get("reps") else 0,
-                distance=float(request.form.get("distance", 0)) if request.form.get("distance") else 0,
-                sets=int(request.form.get("sets", 0)) if request.form.get("sets") else 0,
-                weight=float(request.form.get("weight", 0)) if request.form.get("weight") else 0,
-                duration=float(request.form.get("duration", 0)) if request.form.get("duration") else 0
+                reps=int(request.form.get(
+                    "reps", 0)) if request.form.get("reps") else 0,
+                distance=float(request.form.get(
+                    "distance", 0)) if request.form.get("distance") else 0,
+                sets=int(request.form.get(
+                    "sets", 0)) if request.form.get("sets") else 0,
+                weight=float(request.form.get(
+                    "weight", 0)) if request.form.get("weight") else 0,
+                duration=float(request.form.get(
+                    "duration", 0)) if request.form.get("duration") else 0
             )
             db.session.add(activity)
             db.session.commit()
@@ -137,7 +159,8 @@ def add_activity():
             # cancel any potential changes made to the database
             db.session.rollback()
             # catch the error and flash a message
-            flash(f'Error, failed to add an activity: {str(e)}', category='error')
+            flash(f'Error, failed to add an activity: {str(e)}',
+                category='error')
 
     return render_template("add_activity.html", user=current_user)
 
@@ -152,11 +175,16 @@ def edit_activity(activity_id):
             # attempt to update the database
             activity.workout_type=request.form.get("workout_type"),
             activity.exercise_name=request.form.get("exercise_name"),
-            activity.reps=int(request.form.get("reps", 0)) if request.form.get("reps") else 0,
-            activity.distance=float(request.form.get("distance", 0)) if request.form.get("distance") else 0,
-            activity.sets=int(request.form.get("sets", 0)) if request.form.get("sets") else 0,
-            activity.weight=float(request.form.get("weight", 0)) if request.form.get("weight") else 0,
-            activity.duration=float(request.form.get("duration", 0)) if request.form.get("duration") else 0
+            activity.reps=int(request.form.get(
+                "reps", 0)) if request.form.get("reps") else 0,
+            activity.distance=float(request.form.get(
+                "distance", 0)) if request.form.get("distance") else 0,
+            activity.sets=int(request.form.get(
+                "sets", 0)) if request.form.get("sets") else 0,
+            activity.weight=float(request.form.get(
+                "weight", 0)) if request.form.get("weight") else 0,
+            activity.duration=float(request.form.get(
+                "duration", 0)) if request.form.get("duration") else 0
             db.session.commit()
             flash('Activity updated', category='success')
             return redirect(url_for('diary'))
@@ -165,9 +193,14 @@ def edit_activity(activity_id):
             # cancel any potential changes made to the database
             db.session.rollback()
             # catch the error and flash a message
-            flash(f'Error, failed to edit activity: {str(e)}', category='error')
+            flash(f'Error, failed to edit activity: {str(e)}',
+                category='error')
 
-    return render_template("edit_activity.html", user=current_user, activity=activity)
+    return render_template(
+        "edit_activity.html",
+        user=current_user,
+        activity=activity
+    )
 
 
 # Function to allow user to delete an activity log
@@ -185,12 +218,14 @@ def delete_activity(activity_id):
         # cancel any potential changes made to the database
         db.session.rollback()
         # catch the error and flash a message
-        flash(f'Error, failed to delete activity: {str(e)}', category='error')
+        flash(f'Error, failed to delete activity: {str(e)}',
+            category='error')
 
     return redirect(url_for('diary'))
 
 
-# Function that loads the data from the json file and passes into the exercises_data variable
+# Function that loads the data from the json file
+# passes into the exercises_data variable
 @app.route('/get_exercises')
 @login_required
 def get_exercises():
@@ -201,11 +236,13 @@ def get_exercises():
         return jsonify(exercises_data)
     except (IOError, json.JSONDecodeError) as e:
         # if there is an error loading the json file
-        flash(f'Error, failed to load JSON file: {str(e)}', category='error')
+        flash(f'Error, failed to load JSON file: {str(e)}',
+            category='error')
         return redirect(url_for('diary'))
     except Exception as e:
         # if there is a general error
-        flash(f'Error, failed to load exercises: {str(e)}', category='error')
+        flash(f'Error, failed to load exercises: {str(e)}',
+            category='error')
         return redirect(url_for('diary'))
 
 
@@ -216,10 +253,15 @@ def activity_feed():
     try:
         # attempt to load activity feed
         activities = list(Activity.query.order_by(Activity.created_at).all())
-        return render_template("activity_feed.html", user=current_user, activities=activities)
+        return render_template(
+            "activity_feed.html",
+            user=current_user,
+            activities=activities
+        )
     except Exception as e:
         # catch the error and flash a message, redirect to home page
-        flash(f'Error, failed to load activity feed: {str(e)}', category='error')
+        flash(f'Error, failed to load activity feed: {str(e)}',
+            category='error')
         return redirect(url_for('home'))
 
 
@@ -243,7 +285,8 @@ def add_comment():
             # cancel any potential changes made to the database
             db.session.rollback()
             # catch the error and flash a message
-            flash(f'Error, failed to add comment: {str(e)}', category='error')
+            flash(f'Error, failed to add comment: {str(e)}',
+                category='error')
     return redirect(request.referrer or url_for('activity_feed'))
 
 
@@ -262,7 +305,8 @@ def delete_comment(comment_id):
          # cancel any potential changes made to the database
         db.session.rollback()
         # catch the error and flash a message
-        flash(f'Error, failed to delete comment: {str(e)}', category='error')
+        flash(f'Error, failed to delete comment: {str(e)}',
+            category='error')
     return redirect(request.referrer or url_for('activity_feed'))
 
 
@@ -276,28 +320,48 @@ each error_message variable holds a string value relevant to the error
 @app.errorhandler(400)
 def error_400(e):
     error_message = "The server cannot process the request."
-    return render_template("error.html", user=current_user, error_message=error_message), 400
+    return render_template(
+        "error.html",
+        user=current_user,
+        error_message=error_message
+    ), 400
 
 
 @app.errorhandler(401)
 def error_401(e):
     error_message = "You are not authorized to view that page."
-    return render_template("error.html", user=current_user, error_message=error_message), 401
+    return render_template(
+        "error.html",
+        user=current_user,
+        error_message=error_message
+    ), 401
 
 
 @app.errorhandler(403)
 def error_403(e):
     error_message = "You do not have permission to view that page."
-    return render_template("error.html", user=current_user, error_message=error_message), 403
+    return render_template(
+        "error.html",
+        user=current_user,
+        error_message=error_message
+    ), 403
 
 
 @app.errorhandler(404)
 def error_404(e):
     error_message = "Page not found."
-    return render_template("error.html", user=current_user, error_message=error_message), 404
+    return render_template(
+        "error.html",
+        user=current_user,
+        error_message=error_message
+    ), 404
 
 
 @app.errorhandler(500)
 def error_500(e):
     error_message = "Internal server error."
-    return render_template("error.html", user=current_user, error_message=error_message), 500
+    return render_template(
+        "error.html",
+        user=current_user,
+        error_message=error_message
+    ), 500
